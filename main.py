5fdb895c02262e88bac1b9641c7faa9bb22d70bf
gustavo-sos <gus.oliveira046@gmail.com>
2024-10-07 11:12:36 -0300

    Reddit changed the way Praw locates "praw.ini", th is commit is to adjust to those changes.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import praw
 import time
 from datetime import datetime
@@ -11,24 +12,25 @@ def create_notification_item():
                             msg=f"You have new keys available",
                             duration="short")
             
-    temp_item.add_actions(label="Show Me", launch="file:///C:\\THE_PATH_TO_YOUR_TXT_FILE") # This lib, unfortunately, only accepts abolute paths :c
+    temp_item.add_actions(label="Show Me", launch="file:///- The absolute path to the folder where this file is goes here - \\keys.txt") # This lib, unfortunately, only accepts abolute paths :c
     temp_item.set_audio(audio.Default, loop=False)
     return temp_item
 
 def get_unix_from_10_min_ago():
     current_time = datetime.now()
     unix_now = time.mktime(current_time.timetuple())
-    unix_10_min_ago = unix_now - 600 # This is the part that effectively measures the time between the post date and now, in seconds. Change the value for testing purposes
+    unix_10_min_ago = unix_now - 600 # This is the part that, effectively, measures the time between the post date and now, in seconds. 
+                                     # Default is 600, change the value for testing purposes, 60 = 1 minute, 600 = 10 minutes, 3600 = 1 hour
     return unix_10_min_ago
 
 def scrape_reddit():
     
     unix_10_min_ago = get_unix_from_10_min_ago()
-    regexp = re.compile(r"[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}")
+    regexp = re.compile(r"[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}") # Here you can customize the RegExp, I recommend using "regex101.com" if you're not familiar with manually writing those
     key_list = []
 
     try:
-        reddit = praw.Reddit("bot1", user_agent="windows:teste-keys:v0.1 by /u/dathed-bot") # You should use your own client_id and secret here
+        reddit = praw.Reddit("Your bot goes here.", user_agent="Your user agent goes here.") # You should use your own client_id and secret here
     except praw.exceptions.PRAWException as err:
         print(f"Reddit initialization failed: {err}")
         return
@@ -45,8 +47,9 @@ def scrape_reddit():
     except Exception as err:
         print(err) # Again, just for good measure, as this will probably never get triggered
         return
-
-    for submission in reddit.subreddit("XboxGamePass").new(limit=None):
+    
+    custom_subreddit = "XboxGamePass" # You can change the Subreddit here. It is not case sensitive.
+    for submission in reddit.subreddit(custom_subreddit).new(limit=None):
             
         if submission.created_utc < unix_10_min_ago:
             print("Posts from now on are older than 10 minutes.\nNow it will write the keys and check for duplicates.")
@@ -73,6 +76,7 @@ def scrape_reddit():
         if write_check == 1:
             popup.show()
 
+os.chdir("The absolute path to the folder where this file is goes here.")
 scrape_reddit()
 
 # If you want it to run in the background, use the code below and comment the function call above
